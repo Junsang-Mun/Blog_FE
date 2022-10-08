b40b4d01e0ec04f4b12f000c6fa9352f54ba5976
Junsang Mun <mun.js@aol.com>
2022-10-08 19:43:48 +0900

    [WIP] main page working
    - 파트별로 나누는 중

---
+++ src/routes/MainPage.svelte
@@ -0,0 +1,2 @@
+<h1>junsang.dev</h1>
+<p>My name is Junsang Mun.</p>
---
+++ src/routes/UIShell.svelte
@@ -0,0 +1,44 @@
+<script>
+	import "carbon-components-svelte/css/all.css";
+	import {
+	Header,
+	SideNav,
+	SideNavItems,
+	SideNavLink,
+	SkipToContent,
+	Content,
+	Grid,
+	Row,
+	Column,
+	Theme,
+	} from "carbon-components-svelte";
+
+	let isSideNavOpen = false;
+</script>
+
+<Header company="IBM" platformName="Carbon Svelte" bind:isSideNavOpen>
+	<svelte:fragment slot="skip-to-content">
+	<SkipToContent />
+	</svelte:fragment>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+	<Theme
+		theme="g10"
+	/>
+	<SideNavItems>
+	<SideNavLink text="Home" />
+	<SideNavLink text="Tags" />
+	<SideNavLink text="Link 3" />
+	</SideNavItems>
+</SideNav>
+
+<Content>
+	<Grid>
+	<Row>
+		<Column>
+		<slot></slot>
+		</Column>
+	</Row>
+	</Grid>
+</Content>
