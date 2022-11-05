104ba0ca31bf709cc06ceb819c99a2aaa8f21f2d
Junsang Mun <mun.js@aol.com>
2022-11-05 17:07:34 +0900

    [feature] return ERR when back is dead
    - try-catch로 백이 죽으면 ERR 리턴하도록; timeout 설정 필요할까?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,12 @@
 		data = await postPreview();
 		console.log(data);
 	});
-</script>
-<style>
-	.test {
-		color: var(--orange-8);
+	function redirectUrl(url) {
+		window.open(`http://localhost:5173/post/${url}`);
 	}
+</script>
+
+<style>
 	.preview {
 		background-color: var(--gray-5);
 		margin: var(--size-3);
@@ -21,13 +22,15 @@
 </style>
 
 <h1>악! 음 주코딩이세상에서제일잘돼 해병님!</h1>
-{#if data}
+{#if data === 'ERR'}
+	<p>ERROR</p>
+{:else if data}
 	{#each data.posts as post}
 		<div class="preview">
-			<h2 class="test">{post.title}</h2>
-			<p>{post.tag}</p>
+			<h2>{post.title}</h2>
+			<p id="tag">{post.tag}</p>
 			<p>{post.id}</p>
-			<a href="/post/{post.id}">go to post: {post.title}</a>
+			<button on:click={redirectUrl(post.id)}>go to post: {post.title}</button>
 		</div>
 	{/each}
 {:else}
--- src/scripts/apiCall.js
+++ src/scripts/apiCall.js
@@ -1,12 +1,18 @@
 const url = 'http://localhost:3030/api';
 
 export async function postPreview() {
-	const response = await fetch(`${url}/post`);
-	if (!response.ok) {
-		return 'ERR';
+	try {
+		const response = await fetch(`${url}/post`);
+		if (!response.ok) {
+			return 'ERR';
+		}
+		const returnValue = await response.json();
+		return returnValue;
+	} catch(e) {
+		if (e) {
+			return "ERR";
+		}
 	}
-	const returnValue = await response.json();
-	return returnValue;
 }
 
 export async function postDetail(id) {
